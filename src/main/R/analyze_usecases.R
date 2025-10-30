1dc7df53841848ecd1349e786af93f25b7cea5da
u204352 <thomas.hettinger@sbb.ch>
2025-10-30 14:46:22 +0100

    initial analysis commit

---
+++ src/main/R/analyze_usecases.R
@@ -0,0 +1,10 @@
+rm(list = ls())
+
+
+filer <- '//filer22l/K-UE220L/IFI/FTO/SAM.A13783/'
+
+#baseDirectory = paste0(filer, "04_projects/26_vivaldi_phase3/")
+baseDirectory = paste0(filer, "42_gzb_railsim/output_20251030_ik/")
+
+usecase <- "uc_1"
+usecaseDirectory <- paste0(baseDirectory, usecase, "/")
